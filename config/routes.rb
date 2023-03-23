0d24cdfb99005c37e1dd504a422908c1d5557cd4
felipemanso <desdejulhode78@gmail.com>
2023-03-23 11:47:54 -0300

    final hits on routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   resources :ideas
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: redirect('/ideas')
 end
